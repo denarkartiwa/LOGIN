78af923c9b5b68459670b7c2091f9176c49335e5
Denar Kartiwa <denarkartiwa@Denars-MacBook-Air.local>
2024-05-12 22:43:07 +0900

    Initial commit

---
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btnlogin;
+    private EditText username, password;
+    //isi email dan password
+    private String xusername = "admin";
+    private String xpassword = "admin";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // inisialisasi btnlogin, username, dan password menggunakan findViewById
+        btnlogin = findViewById(R.id.Btnlogin);
+        username = findViewById(R.id.EmailAddress);
+        password = findViewById(R.id.Password);
+
+        // fungsi
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(username.getText().toString().equalsIgnoreCase(xusername) &&
+                        password.getText().toString().equals(xpassword)){
+                    Intent login = new Intent(MainActivity.this, DashboardActivity.class);
+                    startActivity(login);
+                    Toast.makeText(MainActivity.this, "success", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Username or password is incorrect", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+}
